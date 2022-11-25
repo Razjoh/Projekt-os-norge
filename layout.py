a2fb94807575d7b1b15c6df6136ee2aa7357250b
Rasmus Johansson <raz.johansson@gmail.com>
2022-11-25 12:48:13 +0100

    Added more comments

--- layout.py
+++ layout.py
@@ -12,10 +12,12 @@ class Layout:
     def layout(self):
         return dbc.Container(
             [
+                # First header: Norway
                 dbc.Card(
                     dbc.CardBody(html.H1("Norway Olympics stats")), class_name="mt-3"
                 ),
                 dbc.Col(
+                    # Dropdown filter picker first graph 
                     dcc.Dropdown(
                         id="graph-picker",
                         options=[
@@ -27,6 +29,7 @@ class Layout:
                     class_name="mt-2"
                 ),
                 dbc.Col(
+                    # Tabs to pick adjusting for teams first graph
                     dcc.Tabs(
                         id="adjusted",
                         value="Norway",
@@ -47,14 +50,18 @@ class Layout:
                     ),
                     class_name="mt-2"
                 ),
+                # First graph: stats Norway
                 dcc.Graph(id="norway-graph"),
+                # Hide show graph x labels
                 dbc.Card(
                     dbc.Button("Show labels", n_clicks=0, id="labels", color="Dark")
                 ),
+                # Second header: Sports
                 dbc.Card(
                     dbc.CardBody(html.H1("Olympics sports stats")), class_name="mt-3"
                 ),
                 dbc.Col(
+                    # Dropdown filter pick sport second graph
                     dcc.Dropdown(
                         id="sports-picker",
                         options=[
@@ -66,6 +73,7 @@ class Layout:
                     class_name="mt-2"
                 ),
                 dbc.Col(
+                    # Tabs to pick filter medals or gender second graph
                     dcc.Tabs(
                         id="sport-tabs",
                         value="tab-1",
@@ -86,8 +94,10 @@ class Layout:
                     ),
                     class_name="mt-2"
                 ),
+                # Second graph sports medals/gender every olympics
                 dcc.Graph(id="sports-graph"),
                 dbc.Col(
+                    # slider to pick olympics to show
                     dcc.Slider(
                         id="game-slider",
                         min=0,
